5d7a7fbab653b9113d246df1ccbd31919e9d4459
tkyk.niimura <tkyk.niimura@gmail.com>
2021-08-22 22:48:51 +0900

    fix test code

---
+++ ManualTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ManualTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ManualTest()
+        {
+            // Show form, and test manualy
+            TestTargetExeForm.Program.Main();
+        }
+    }
+}
